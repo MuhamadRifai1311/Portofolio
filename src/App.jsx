c3dd36b612df620236f7c343d769a4ad877f0a08
muhamadrifai0210 <mr209488@gmail.com>
2025-04-19 20:37:41 +0700

    perbaikan

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./assets/Component/Header";
-import About from "./assets/Component/About";
-import MyProject from "./assets/Component/MyProject";
+import Header from "./assets/component/Header";
+import About from "./assets/component/About";
+import MyProject from "./assets/component/MyProject";
 import Content from "./assets/Component/content";
-import ScrollToTop from "./assets/Component/scrollToTop";
+import ScrollToTop from "./assets/component/scrollToTop";
 import DS from "./assets/Component/Projects/Ds";
 
 function App() {
