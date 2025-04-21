d2a73acdb8a099ddfdf4538dbb76321be54abbda
muhamadrifai0210 <mr209488@gmail.com>
2025-04-21 18:54:08 +0700

    perbaikan 2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/Portofolio/",
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' && import.meta.env.VITE_GITHUB_PAGES
+    ? '/Portofolio/'
+    : '/',
   plugins: [react()],
-});
+}));
+
